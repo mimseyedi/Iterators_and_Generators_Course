6c39bae842eacc1b445e0ce1b9fa1e32deedffb3
Mohammad <mim.seyedi@gmail.com>
2025-03-27 14:26:24 +0330

    Create combinations.py
    Implementation of the 'combinations' Generator.

---
+++ 8_itertools/8_3_Combinatoric_Iterators/8_3_3_combinations/combinations.py
@@ -0,0 +1,21 @@
+from typing import Iterable, Generator
+
+
+def combinations(iterable: Iterable, r: int) -> Generator:
+    pool = tuple(iterable)
+    n = len(pool)
+    if r > n:
+        return
+    indices = list(range(r))
+
+    yield tuple(pool[i] for i in indices)
+    while True:
+        for i in reversed(range(r)):
+            if indices[i] != i + n - r:
+                break
+        else:
+            return
+        indices[i] += 1
+        for j in range(i+1, r):
+            indices[j] = indices[j-1] + 1
+        yield tuple(pool[i] for i in indices)
