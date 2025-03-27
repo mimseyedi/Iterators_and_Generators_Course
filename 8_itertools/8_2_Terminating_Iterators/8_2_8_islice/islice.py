3462fa159408280ddc56d1551034b68358fdf828
Mohammad <mim.seyedi@gmail.com>
2025-03-27 14:26:02 +0330

    Create islice.py
    Implementation of the 'islice' Generator.

---
+++ 8_itertools/8_2_Terminating_Iterators/8_2_8_islice/islice.py
@@ -0,0 +1,18 @@
+from typing import Generator, Iterable
+from itertools import count
+
+
+def islice(iterable: Iterable, *args: int | None) -> Generator:
+    s = slice(*args)
+    start = 0 if s.start is None else s.start
+    stop = s.stop
+    step = 1 if s.step is None else s.step
+    if start < 0 or (stop is not None and stop < 0) or step <= 0:
+        raise ValueError("islice does not accept negative numbers")
+
+    indices = count() if stop is None else range(max(start, stop))
+    next_i = start
+    for i, element in zip(indices, iterable):
+        if i == next_i:
+            yield element
+            next_i += step
