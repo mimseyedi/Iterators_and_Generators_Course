4f2b9b2f8df00088763a2d41c3518dcf1aea5b09
Mohammad <mim.seyedi@gmail.com>
2025-03-27 14:09:04 +0330

    Create sequential.py
    Implementing 'Sequential' Processing.

---
+++ 6_Generator-based_Coroutines/6_1_Introduction/sequential.py
@@ -0,0 +1,15 @@
+def a() -> None:
+    print("Function 'A' executed!")
+
+
+def b() -> None:
+    print("Function 'B' executed!")
+
+
+def c() -> None:
+    print("Function 'C' executed!")
+
+    
+a()
+b()
+c()
