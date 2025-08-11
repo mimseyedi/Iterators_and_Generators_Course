53f29b29b18fa246863d3dfd1b3d8c5ed4dbefcb
Mohammad <mim.seyedi@gmail.com>
2025-08-11 17:19:58 +0330

    Create roundrobin.py

---
+++ 9_Additionals/9_4_more-itertools/roundrobin.py
@@ -0,0 +1,19 @@
+from itertools import cycle, islice
+
+
+def roundrobin(*iterables):
+    """Visit input iterables in a cycle until each is exhausted.
+
+        >>> list(roundrobin('ABC', 'D', 'EF'))
+        ['A', 'D', 'E', 'B', 'F', 'C']
+
+    This function produces the same output as :func:`interleave_longest`, but
+    may perform better for some inputs (in particular when the number of
+    iterables is small).
+
+    """
+    # Algorithm credited to George Sakkis
+    iterators = map(iter, iterables)
+    for num_active in range(len(iterables), 0, -1):
+        iterators = cycle(islice(iterators, num_active))
+        yield from map(next, iterators)
